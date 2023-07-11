53bc37c30e9393a507cf05183aa32910924accca
Hk-Ko <htunkko.mgk@gmail.com>
2023-07-11 12:03:14 +0630

    bg fixed

--- src/components/Charts/Table.jsx
+++ src/components/Charts/Table.jsx
@@ -14,7 +14,7 @@ const Table = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-between gap-5 p-5 xl:w-[48%] 2xl:w-[49%] bg-secondary rounded-xl">
+    <div className="flex flex-col items-center justify-between gap-5 p-5 xl:w-[48%] 2xl:w-[49%] category-bg rounded-xl">
       <div className="flex items-center justify-between w-full">
         <div>
           <h5 className="text-xl font-semibold text-heading_text">Activity</h5>
@@ -57,7 +57,7 @@ const Table = () => {
               <th>Time</th>
             </tr>
           </thead>
-          <tbody >
+          <tbody>
             {activities.map((activity) => (
               <tr key={activity.id}>
                 <td className="px-5 h-[58px] border-b border-t border-l border-gray-700 ">
